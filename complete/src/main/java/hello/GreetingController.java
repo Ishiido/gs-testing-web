2c8498bc186eb3db075d97091244fb4c241443bc
Florian Steinborn <florian.steinborn@ub.hu-berlin.de>
2018-01-02 20:44:05 +0100

    + Singleton Tests

--- complete/src/main/java/hello/GreetingController.java
+++ complete/src/main/java/hello/GreetingController.java
@@ -9,14 +9,38 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class GreetingController {
 
     private final GreetingService service;
+    private final SingletonService sS;
+    
 
     public GreetingController(GreetingService service) {
         this.service = service;
+        this.sS = null;
     }
+    
+    public GreetingController(SingletonService service) {
+        this.sS = service;
+        this.service = null;
+    }
+
+    
 
     @RequestMapping("/greeting")
     public @ResponseBody String greeting() {
         return service.greet();
     }
 
+    @RequestMapping ("/returnString")
+    public @ResponseBody String returnString()
+    {
+        String retVal = "";
+        SingletonService singletonService = SingletonService.getInstance("/var/tmp/config");
+        if (singletonService==null)
+        {
+            retVal = "SingletonServcie not initialised";
+        } else {
+            retVal = singletonService.returnAString();
+        }
+        return retVal;
+    }
+    
 }
